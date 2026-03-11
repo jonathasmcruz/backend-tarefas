a0a3999747427708970bc9f7c459cbc7994d6113
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-03-11 09:43:10 -0300

    Add files via upload

---
+++ hora.js
@@ -0,0 +1,12 @@
+let valorPorHora = 50;
+let HorasTrabalhadas = 8;
+
+
+console.log("--- Resumo do Expediente ---")
+console.log(`Horas Iniciais: ${HorasTrabalhadas}h`)
+
+HorasTrabalhadas++;
+console.log(`Após hora extra (horasTrabalhadas++): ${HorasTrabalhadas}h`);
+
+HorasTrabalhadas--;
+console.log(` Após saída antecipada: ${HorasTrabalhadas}h`);
