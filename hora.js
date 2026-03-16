02efc5fe822f5d460291aa380ddd4e5de8604615
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-03-16 13:54:11 +0000

    versao1

--- hora.js
+++ hora.js
@@ -10,3 +10,9 @@ console.log(`Após hora extra (horasTrabalhadas++): ${HorasTrabalhadas}h`);
 
 HorasTrabalhadas--;
 console.log(` Após saída antecipada: ${HorasTrabalhadas}h`);
+
+HorasTrabalhadas--
+console.log(`Após saída antecipadas (horasTrabalhadas--): ${horasTrabalhadas}h`);
+
+let taxaSistema = 15;
+let pagamentoTotal = (horasTrabalhadas * valorPorHora) - taxaSistema;
