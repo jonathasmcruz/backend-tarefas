2005d1240de7aba6f1524d9436e7cfd81a5fecd4
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-03-17 11:51:42 +0000

    versao1

---
+++ string.js
@@ -0,0 +1,12 @@
+onsole.log("ENDEREÇO COMPLETO");
+
+let rua = "Rua das Flores";
+let numero = 9851;
+let cidade = "Curitiba";
+let cep = "80010-000";
+
+let enderecoCompleto = rua + "," + numero + "-" + cidade;
+
+console.log(enderecoCompleto);
+
+//Adicione informações adicionais
