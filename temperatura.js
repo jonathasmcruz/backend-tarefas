303acb9869ecce63267200b999609c333418a0a7
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-03-23 13:50:15 +0000

    versao4

--- temperatura.js
+++ temperatura.js
@@ -5,8 +5,19 @@ const tempFahreheint = tempCelsius * (9 / 5 + 32);
 const tempKelvin= tempCelsius + 273.15;
 const variacaoDiaria = 26.3 - tempCelsius;
 
-console.log(`Temperatura em ${cidade} hoje está ${tempCelsius}.`);
-console.log(`Celsius: ${tempCelsius}`);
+console.log(`Temperatura em ${cidade} hoje está ${tempCelsius}`);
+console.log(`Celsius: ${tempCelsius} Cº`);
 
-console.log(`Temperatura em ${cidade} hoje está ${tempFahreheint}.`);
-console.log(`Temperatura em ${cidade} hoje está ${tempKelvin}.`);
+console.log( )
+
+console.log(`Temperatura em ${cidade} hoje está ${tempFahreheint}`);
+console.log(`Fahreheint: ${tempFahreheint} Fº`);
+
+console.log( )
+
+console.log(`Temperatura em ${cidade} hoje está ${tempKelvin}`);
+console.log(`Kelvin: ${tempKelvin} Kº`);
+
+console.log( )
+
+console.log(`\nVarição prevista ${variacaoDiaria.toFixed(1)}  Cº`);
