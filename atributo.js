c714c07bef67570242e7832b55b77c4d1a653984
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-03-24 13:53:28 +0000

    versao4

--- atributo.js
+++ atributo.js
@@ -8,8 +8,11 @@ numeroN *= 2;
 console.log(numeroN);
 
 const idadeDandara = 26;
-const Minhaidade = 67;
+const Minhaidade = 17;
 
 
 const Saoiguais = idadeDandara == Minhaidade;
 console.log('As idades são iguais? ' + Saoiguais )
+
+const comparacao = idadeDandara > Minhaidade;
+console.log('A dandara é mais velha ?' + comparacao);
