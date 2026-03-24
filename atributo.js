73715361b86e14ecd527cfaa8b6c7fb4e0078578
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-03-24 13:47:32 +0000

    versao4

---
+++ atributo.js
@@ -0,0 +1,15 @@
+let numero = 2;
+numero = numero * 2;
+
+console.log(numero);
+
+let numeroN = 2;
+numeroN *= 2;
+console.log(numeroN);
+
+const idadeDandara = 26;
+const Minhaidade = 67;
+
+
+const Saoiguais = idadeDandara == Minhaidade;
+console.log('As idades são iguais? ' + Saoiguais )
