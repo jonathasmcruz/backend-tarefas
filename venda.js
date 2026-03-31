d3d3c30ed06e95b411bb7b6fb9a74e3bad10387c
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-03-31 14:41:45 +0000

    versao1

---
+++ venda.js
@@ -0,0 +1,13 @@
+let nomeCliente = "Wellington";
+let tipoCliente = "Sabor repositor";
+
+let nomeProduto = "Azeite de oliva";
+let precoProduto = "nao sou obrigado a saber";
+let estoque = 10;
+let quantidadePedido = 9;
+
+let estoqueOK = quantidadePedido <= estoque;
+
+if (estoqueOK) {
+    console.log()
+}
