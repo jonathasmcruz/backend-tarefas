803836cdce9ed73d977afc50888b53db199ea5af
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-04-07 11:53:04 +0000

    versao1

---
+++ pontuacao.js
@@ -0,0 +1,8 @@
+const nomeJogador = "Mussolini";
+let faseAtual = 4;
+let vidasRestantes = 4;
+
+let PontosPorMissao = [85, 90, 67, 100];
+
+console.log('Seja bem vindo ${nomeJogador}!')
+console.log(PontosPorMissao);
