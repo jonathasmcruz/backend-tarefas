3203ed8293c7f0bd017549424f5e554b919297ad
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-04-08 12:32:48 +0000

    versao1

--- pontuacao.js
+++ pontuacao.js
@@ -4,5 +4,16 @@ let vidasRestantes = 4;
 
 let PontosPorMissao = [85, 90, 67, 100];
 
+let pontuacaoTotal = 0;
+
+for (let i = 0; i < pontuacoes.lenght; i++){
+  pontuacaoTotal = pontuacaoTotal + pontuacoes[i]
+}
+
+let media = pontuacaoTotal / pontuacoes.lenght;
+
+let passouDeFase = (media >= 75 && vidas > 0) || (fase === 1)
+    .
+
 console.log('Seja bem vindo ${nomeJogador}!')
 console.log(PontosPorMissao);
