1979f58ce1d8ab63ed63c8732dabdbf9d7942384
jonathasmcruz <jonathas.cruz.junior@escola.pr.gov.br>
2026-05-06 12:31:56 +0000

    versao3

--- cardapio.js
+++ cardapio.js
@@ -14,14 +14,33 @@ console.log(cardapio[2].categoria);
 
 function exibirCardapio(lista) {
     console.log("===CARDÁPIO===")
-    console.log(`1. ${lista[0].nome} - R$ ${lista[0].preco}`)
-    console.log(`2. ${lista[1].nome} - R$ ${lista[1].preco}`)
-    console.log(`3. ${lista[2].nome} - R$ ${lista[2].preco}`)
-    console.log(`4. ${lista[3].nome} - R$ ${lista[3].preco}`)
-    console.log(`5. ${lista[4].nome} - R$ ${lista[4].preco}`)
+    console.log(`1. ${lista[0].nome} - R$ ${lista[0].categoria}`)
+    console.log(`2. ${lista[1].nome} - R$ ${lista[1].categoria}`)
+    console.log(`3. ${lista[2].nome} - R$ ${lista[2].categoria}`)
+    console.log(`4. ${lista[3].nome} - R$ ${lista[3].categoria}`)
+    console.log(`5. ${lista[4].nome} - R$ ${lista[4].categoria}`)
 }
 
 function calcularTotal(listaPedido) {
     let total = listaPedido[0].preco + listaPedido[1].preco;
     return total;
 }
+
+function calcuclarComDesconto(total, clienteObj) {
+    if (clienteObj.fidelidade === true) {
+        let desconto = total * 0.10;
+        console.log(``Desconto fidelidade: R${desconto}`);
+        return total - desconto
+    }
+    return total;
+}
+
+function exibirPedido(listaPedido){
+    console.log("===SEU PEDIDO===");
+    console.log(`1. ${listaPedido[0].nome} - R$ ${listaPedido[0].preco}`);
+    console.log(`2. ${listaPedido[1].nome} - R$ ${listaPedido[1].preco}`);
+    console.log(`TOTAL: R$ ${calcularTotaL(listaPedido)}`);
+}
+
+exibirCardapio(cardapio);
+exibirPedido(pedido);
